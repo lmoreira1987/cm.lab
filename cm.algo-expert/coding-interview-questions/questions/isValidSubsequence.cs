e9c6bebdc0ab86a768f800e3ea67e82ebbcce999
Leonardo Moreira <leonardo.moreira1987@hotmail.com>
2025-03-05 12:04:28 -0300

    IsValidSubsequence

--- cm.algo-expert/coding-interview-questions/questions/isValidSubsequence.cs
+++ cm.algo-expert/coding-interview-questions/questions/isValidSubsequence.cs
@@ -1,27 +1,49 @@
 public class IsValidSubsequenceProblem {
+  // space O(1) | time O(n)
+  // public static bool IsValidSubsequence(List<int> array, List<int> sequence) {
+  //   int pointer =  0;
+  //   int subsequenceCounter = 0;
+  //   int sequenceCount = sequence.Count;
+  //   int arrayCount = array.Count;
+
+  //   if (arrayCount < sequenceCount) 
+  //     return false;
+
+  //   for (int i = 0; i < sequenceCount; i++)
+  //   {
+  //     while(pointer < arrayCount) 
+  //     {
+  //       if (array[pointer] == sequence[i])
+  //       {
+  //         subsequenceCounter++;
+  //         pointer++;
+  //         break;
+  //       }
+
+  //       pointer++;
+  //     }
+  //   }
+
+  //   return subsequenceCounter == sequenceCount;
+  // }
+
+  // space O(1) | time O(n)
+  // With only one loop
   public static bool IsValidSubsequence(List<int> array, List<int> sequence) {
     int pointer =  0;
     int subsequenceCounter = 0;
     int sequenceCount = sequence.Count;
     int arrayCount = array.Count;
 
-    if (arrayCount < sequenceCount) 
-      return false;
-
-    for (int i = 0; i < sequenceCount; i++)
-    {
-      while(pointer < arrayCount) 
+    while(pointer < arrayCount && subsequenceCounter < sequenceCount) 
+    {        
+      if (array[pointer] == sequence[subsequenceCounter])
       {
-        if (array[pointer] == sequence[i])
-        {
-          subsequenceCounter++;
-          pointer++;
-          break;
-        }
-
-        pointer++;
-      }
-    }
+        subsequenceCounter++;
+      }  
+          
+      pointer++;
+    }    
 
     return subsequenceCounter == sequenceCount;
   }
